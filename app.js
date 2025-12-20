779172174e0ebac7739b1cf1b8fd32daa68e62f2
Aadhiesec1 <aadhiesec+git@gmail.com>
2025-12-20 10:57:52 +0000

    Enabled Automatic build via webhook

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("CI/CD Automatic Build Pipeline Working!");
+  res.send("CI/CD Automatic Build Pipeline via jenkins!");
 });
 
 app.listen(3000, '0.0.0.0');

d8a87cb14726fcb502ff4a9f370a046fd9fbc3f1
Aadhiesec1 <aadhiesec+git@gmail.com>
2025-12-22 10:47:24 +0000

    Modiefied index.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("CI/CD Automatic Build Pipeline via github + jenkins!");
+  res.send("CI/CD Automatic Build Pipeline via github + jenkins");
 });
 
 app.listen(3000, '0.0.0.0');

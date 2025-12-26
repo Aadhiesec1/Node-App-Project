5d8f675fd744e3c1729171cec315894ad0e5915c
Aadhiesec1 <aadhiesec+git@gmail.com>
2025-12-26 10:50:19 +0000

    Modified index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Automated CI/CD Pipeline with <b>Github & Jenkins & AWS</b> <3");
+  res.send("Automated CI/CD Pipeline with <b>Github & Jenkins & AWS <3 </b>");
 });
 
 app.listen(PORT, () => {
